21eaffc7b9b9386eeebfd4494021f261af27af5b
stephanogiuseppe <stephano.grp@gmail.com>
2020-01-01 23:15:07 -0300

    first commit - initial configuration

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: false,
+  semi: false
+};
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import './config/ReactotronConfig'
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>Welcome to my app!</Text>
+      </View>
+    </>
+  )
+}
+
+export default App
