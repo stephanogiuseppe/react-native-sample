e2d2cfcf33a16dfb71d7b58d4424e60804dc8174
stephanogiuseppe <stephano.grp@gmail.com>
2020-01-02 01:01:11 -0300

    add material icons and simple form in home

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import { Container } from './styles'
+import { Container, Form, Input, SubmitButton } from './styles'
 
 export default function Main() {
-  return <Container />
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+        />
+
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  )
 }
 
 Main.navigationOptions = {
